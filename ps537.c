36e20f596aafab18d79eb48016a9eefed6ae1583
WillHofkamp <50555559+WillHofkamp@users.noreply.github.com>
2020-09-07 22:50:44 -0500

    Initial Commit
    Initial commit of README and code

---
+++ ps537.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+/*
+* This method will loop through all the pids and print out a process status for each process
+*/
+void printStatus(int *pid, int stateInformationFlag, int userTimeFlag, int systemTimeFlag, int virtualMemoryFlag, int commandLineFlag) {
+  for (int i = 0; i < (pid size); ++i) {
+	if(stateInformationFlag) {
+	    //char stateInfoChar = find way to get the single-character state information about the process. This information is found in the stat file in
+	    //process's directory, looking at the third ("state") field. Note that the information that you read from the stat file is a charcter string.
+	    //This option defaults to false aka 0 
+	}
+	if(userTimeFlag) {
+	    //userTime = find way to display the amount of user time consumed by this process. This information is found in the stat file in process's directory,
+            //looking at the "utime" field. This option defaults to true aka 1	
+	}
+	if(systemTimeFlag) {
+	    //systemTime = find way to get the amount of system time consumed so far by this process.
+	    //this information is found in the stat file in process's directory, looking at the "stime" field.
+            //this option defaults to false aka 0
+	}
+	if(virtualMemoryFlag) {
+	    //virtualMemory = find way to get the amount of virtual memory currently being used (in pages) by this program. This information is 
+	    //found in the statm file in process's directory, looking at first ("size") field. This option defaults to false aka 0 	
+	}
+	if(commandLineFlag) {
+	    //commandLine = find way to get the command-line that started this program. This information is found in the cmdline file in process's directory.
+	    //Be careful on this one, because this file contains a list of null (zero byte) terminated strings.
+	}
+  }
+}
+
+/*
+* Main method of program, first processes the command line args, then prints out process info
+*
+*/
+int main(int argc, char **argv) {
+    int pid[];
+    int stateInformationFlag = 0;
+    int userTimeFlag = 1;
+    int systemTimeFlag = 0;
+    int virtualMemoryFlag = 0;
+    int commandLineFlag = 1;
+    char commandLine[];
+    //the arguments are processed in order from left to right
+    for (int i = 0; i < argc; ++i) {
+	if(i = 1) {
+	    if(argv[1] == "-p") {
+		pid[1] = argv[1];
+	    } else {
+	  	//pid[] = get all current user process's pids and put in array
+	    }
+	}
+	else if(argv[i] == "-s") {
+            //enables displaying the single char state information
+	    stateInformationFlag = 1;   
+	    	
+	}
+	else if(argv[i] == "-U-") {
+	    //disables displaying of the user time
+	    userTimeFlag = 0;
+	}
+	else if(argv[i] == "-S") {
+            //enables displaying the amount of system time consumed so far by process
+	    systemTimeFlag = 1;	
+	}
+	else if(argv[i] == "-v") {
+	    //enables displaying the amount of virtual memory a process is using
+	    virtualMemoryFlag = 1;
+	}
+	else if(argv[i] == "-c-") {
+	    //disables displaying of the command line of the process
+	    commandLineFlag = 0;	
+	}
+    }
+    printStatus(pid, stateInformation, userTime, systemTime, virtualMemory, commandLineFlag);
+}
