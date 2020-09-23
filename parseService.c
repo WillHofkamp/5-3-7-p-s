5714aa6d2ebd79e9afbffcca6d0e0c01fb03ef79
pgowni <65507706+pgowni@users.noreply.github.com>
2020-09-23 13:18:37 -0500

    Update parseService.c

--- parseService.c
+++ parseService.c
@@ -18,12 +18,13 @@
 // This method goes through each command line arg and checks if it matches up 
 // to one of the possible commands, and then sets the designated flag for that
 // as well as retrieving a possible process id.
-CmdLineArgDto * parseCommandLine(int argc, char *argv[]){
+cmdLineArgs * parse(int argc, char *argv[]){
   
-	// hold the selected option
+	// variable for the chosen option from the command line
 	int option;
+	int argsCount = 0;
  
-	// flags to keep track which of the commands were slected
+	// flags to keep track which of the commands were selected
 	//-p
 	int pFlag = 0;
 	// -s
@@ -36,119 +37,125 @@ CmdLineArgDto * parseCommandLine(int argc, char *argv[]){
 	int vFlag = 0;
 	// -c
 	int cFlag = 1;
-
-	int numOptions = 0;
-
-	// store the process id if one is specified in the command line arguments
+	// if no process id is given in the command line arguments
 	char *pID = NULL;
 
 	int i = 0;
 	for(int i = 0; i < argc; i++) {
 		// get options from the command line
 		option = getopt (argc, argv, "p::s::U::S::v::c::");
-		// checks that option was rendered correctly
 		if(option != -1){
+			argsCount++;
 			// -p option
 			if(option == 'p') {
-				//test that there is an extra hyphen
-				if((optarg != NULL) && (strcmp(optarg, "-") == 0)){
+				//checking for the hyphen
+				argsCount++;
+				if(optarg != NULL){
 					// -p- option
-					pFlag = 0;
-				}else if((optarg != NULL)){
-					//test that the extra argument is a number
-						pID = optarg;
+					if(strcmp(optarg, "-" ) == 0){
+						pFlag = 0;
+					}
+				    else{
+					//checking that the optional argument has been provided
+						pID = atoi(optarg);
 						pFlag = 1;
-	  			}else{
-	    				pFlag = 1;
+	  			}}
+				else{
+					printf("No argument provided")
+	    			pFlag = 1;
 				}
 			}
 			// -s option
 			else if(option == 's') {
-				if((optarg != NULL) && (strcmp(optarg, "-") == 0)){
-					// -s- option
-					lSFlag = 0;
-				}else if((optarg != NULL)){
-					printf("ERROR: Process ID list syntax error\n");
-					// error
-					return NULL;
+				if((optarg != NULL){
+					if(strcmp(optarg, "-") == 0){
+						lSFlag = 0;
+					}
+			        else{
+						printf("ERROR: Invalid argument\n");
+						return NULL;
+					}
 				}else{
 					lSFlag = 1;
 				}
 			}
 			// -U option
 			else if(option == 'U') {
-				if((optarg != NULL) && (strcmp(optarg, "-") == 0)){
-					// -U- option
-					uFlag = 0;
-				}else if((optarg != NULL)){
-					printf("ERROR: Process ID list syntax error\n");
-					// error
-					return NULL;
+				if((optarg != NULL){
+					if(strcmp(optarg, "-") == 0){
+						uFlag = 0;
+					}
+			        else{
+						printf("ERROR: Invalid argument\n");
+						return NULL;
+					}
 				}else{
 					uFlag = 1;
 				}
 			}
 			// -S option
 			else if(option == 'S') {
-				if((optarg != NULL) && (strcmp(optarg, "-") == 0)){
-					// -S- option
-					uSFlag = 0;
-				}else if((optarg != NULL)){
-					printf("ERROR: Process ID list syntax error\n");
-					// error
-					return NULL;
+				if((optarg != NULL){
+					if(strcmp(optarg, "-") == 0){
+						uSFlag = 0;
+					}
+			        else{
+						printf("ERROR: Invalid argument\n");
+						return NULL;
+					}
 				}else{
 					uSFlag = 1;
+				}
 				}
 			}
 			// -v option
 			else if(option == 'v') {
-				if((optarg != NULL) && (strcmp(optarg, "-") == 0)){
-					// -v- option
-					vFlag = 0;
-				}else if((optarg != NULL)){
-					printf("ERROR: Process ID list syntax error\n");
-					// error
-					return NULL;
+				if((optarg != NULL){
+					if(strcmp(optarg, "-") == 0){
+						vFlag = 0;
+					}
+			        else{
+						printf("ERROR: Invalid argument\n");
+						return NULL;
+					}
 				}else{
 					vFlag = 1;
 				}
 			}
 			// -c option
 			else if(option == 'c') {
-				if((optarg != NULL) && (strcmp(optarg, "-") == 0)){
-					// -c- option
-					cFlag = 0;
-				}else if((optarg != NULL)){
-					printf("ERROR: Process ID list syntax error\n");
-					// error
-					return NULL;
+				if((optarg != NULL){
+					if(strcmp(optarg, "-") == 0){
+						cFlag = 0;
+					}
+			        else{
+						printf("ERROR: Invalid argument\n");
+						return NULL;
+					}
 				}else{
 					cFlag = 1;
 				}
+
 			} else if(option == '?') {
 				printf("ERROR: Process ID list syntax error\n");
 				return NULL;
 			}
 		}
-		numOptions++;
+		else{
+			break;
+		}
+		//argsCount++;
 	}
 
-	// count the number of arguments left that aren't options
-	int numNonOptions = (argc-numOptions)-1;
-	// there should only be the pid that isn't an option, if it exists
-	if(numNonOptions == 1){
-		// check it's number
-		pID = argv[optind];
-	}else if(numNonOptions > 1){
-		printf("ERROR: Only one id can be specified when using -p.\n");
-		// error
-		return NULL;
+	// check the total number of arguments given
+	if(argc > argsCount){
+		printf("Invalid number of arguments\n")
+	}
 	}
 
 	// construct dto and set values
-	CmdLineArgDto *cmdLineArguments;
-	cmdLineArguments = (CmdLineArgDto*) calloc(1, sizeof(pID)+sizeof(CmdLineArgDto));
+	CmdLineArgs *cmdLineArguments;
+	cmdLineArguments = (CmdLineArgs*) calloc(1, sizeof(pID)+sizeof(CmdLineArgs));
 	cmdLineArguments->pFlag = pFlag;
 	cmdLineArguments->lSFlag = lSFlag;
 	cmdLineArguments->uFlag = uFlag;
@@ -159,4 +166,3 @@ CmdLineArgDto * parseCommandLine(int argc, char *argv[]){
 
 	return cmdLineArguments;
 }
-
